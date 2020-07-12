1141b14172a357f46f9c5ded5bb2dec9faf04bfd
matumotoonga <matumoto.onga@gmail.com>
2020-07-12 20:13:02 +0900

    basic.go -> main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+import(
+	"bufio"
+	"io"
+	"os"
+)
+const PROMPT = "* "
+
+
+func do(in io.Reader, out io.Writer) {
+    for {
+		io.WriteString(out, PROMPT)
+		
+		scanner := bufio.NewScanner(in)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+
+
+	}
+}
+func main(){
+	do(os.Stdin, os.Stdout)
+}
